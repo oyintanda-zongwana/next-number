51c1929cd5870d50d1008db06062f2236c834b8f
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-04 16:40:19 +0200

    done

--- script/index.js
+++ script/index.js
@@ -1,11 +1,11 @@
 let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let index = 0;
-let savedIndex = localStorage.getItem('index');
-if (savedIndex !== null) {
+let savedIndex = localStorage.getItem('index'); //save to local storage
+if (savedIndex !== null) { //If it's not null, it means a valid value was retrieved from local storage.
     index = parseInt(savedIndex, 10); 
 } 
 
-function count(array) {
+function count(array) { //the values are taken from the local storage
     const displayElement = document.getElementById('numberDisplay');
     if (index < array.length) {
         displayElement.innerText = array[index];
@@ -16,7 +16,7 @@ function count(array) {
         localStorage.removeItem('index');  // Reset when all numbers have been printed
     }
 }
-
+//for the button to function
 document.getElementById('nextButton').addEventListener('click', () => {
     count(num);
 });
